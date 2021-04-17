9ab1591c3cddafc30b22d0db735128f8804a7a7b
Kuba <75746226+Whisperer8526@users.noreply.github.com>
2021-04-17 18:30:57 +0200

    Update Tuning-hyperparameters.py

--- Tuning-hyperparameters.py
+++ Tuning-hyperparameters.py
@@ -12,9 +12,9 @@ random_search.fit(scaled_X_train, y_train)
 
 from sklearn.svm import SVC                                             #Randomized Search Cross-validation
 from sklearn.metrics import classification_report, confusion_matrix 
-from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
 param_grid = {'C': [0.1,1, 10, 100], 
               'gamma': [1,0.1,0.01,0.001],
               'kernel': ['rbf']}
-random_search = RandomizedSearchCV(SVC(),param_grid,refit=True,verbose=2, cv=3)
-random_search.fit(scaled_X_train, y_train)
+grid = RandomizedSearchCV(SVC(),param_grid,refit=True,verbose=2, cv=3)
+grid.fit(scaled_X_train, y_train)
