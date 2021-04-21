c40b49e020eb8f29b4a8fdbf42e7d489ee900ae8
Kuba <75746226+Whisperer8526@users.noreply.github.com>
2021-04-21 22:50:58 +0200

    Create Neural Network.py

---
+++ Neural Network.py
@@ -0,0 +1,26 @@
+from tensorflow import keras
+
+model = keras.models.Sequential()
+model.add(keras.layers.Flatten(input_shape=[224,224]))
+model.add(keras.layers.Dense(300, activation="tanh"))
+model.add(keras.layers.Dense(100, activation="relu"))
+model.add(keras.layers.Dense(5, activation="softmax"))
+
+model.compile(loss="sparse_categorical_crossentropy",
+             optimizer="sgd",
+             metrics=["accuracy"])
+
+import numpy as np
+import os
+X = np.load(os.path.join(directory, "image_arrays_bw.npy"))
+y = np.load(os.path.join(directory, "image_labels.npy"))
+
+y= y-1                    #adjusting labels
+
+X_train = X[:2100, :]     #train-test split
+X_test = X[2100:, :]
+y_train = y[:2100]
+y_test = y[2100:]
+
+history = model.fit(X_train, y_train, epochs=100,
+                   batch_size=32, validation_split=0.1)
