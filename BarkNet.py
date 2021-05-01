3eb77666869f0e3591442f5c5fb898207f152a86
Kuba <75746226+Whisperer8526@users.noreply.github.com>
2021-05-01 14:51:34 +0200

    Update BarkNet.py

--- BarkNet.py
+++ BarkNet.py
@@ -62,3 +62,46 @@ BarkNet1_2.add(RegularDense(14))
 BarkNet1_2.add(keras.layers.Dropout(0.3))
 BarkNet1_2.add(keras.layers.Dense(5, activation="softmax",
                                   kernel_initializer="glorot_uniform"))
+
+# BarkNet v1.3
+# This network doesn't show major overfitting symptoms unlike its predecessors
+# [train_accuracy = 0.97, valid_accuracy = 0.90] 15 epochs
+# Learning rate has been changed every 5 epochs  0.003 -> 0.001 -> 0.0001
+
+
+from functools import partial
+
+RegularDense = partial(keras.layers.Dense,
+                       activation="elu", 
+                       kernel_initializer="he_normal",
+                       kernel_regularizer=keras.regularizers.l2(0.01))
+
+from tensorflow.keras.optimizers import Nadam
+
+BarkNet1_2.compile(loss="sparse_categorical_crossentropy",
+             optimizer=Nadam(learning_rate=0.0001),
+             metrics=["accuracy"])
+
+BarkNet1_2 = keras.models.Sequential()
+BarkNet1_2.add(keras.layers.InputLayer(input_shape=[224,224,3]))
+BarkNet1_2.add(keras.layers.BatchNormalization())
+BarkNet1_2.add(keras.layers.ZeroPadding2D(padding=3))
+BarkNet1_2.add(keras.layers.Conv2D(filters=32, kernel_size=(7,7), data_format='channels_last'))
+BarkNet1_2.add(keras.layers.MaxPooling2D())
+BarkNet1_2.add(keras.layers.Conv2D(filters=32, kernel_size=(5,5), data_format='channels_last', padding="same"))
+BarkNet1_2.add(keras.layers.MaxPooling2D())
+BarkNet1_2.add(keras.layers.Conv2D(filters=32, kernel_size=(3,3), data_format='channels_last', padding="same"))
+BarkNet1_2.add(keras.layers.MaxPooling2D())
+
+BarkNet1_2.add(keras.layers.Flatten())
+BarkNet1_2.add(keras.layers.BatchNormalization())
+BarkNet1_2.add(RegularDense(28))
+BarkNet1_2.add(keras.layers.BatchNormalization())
+BarkNet1_2.add(RegularDense(28))
+BarkNet1_2.add(keras.layers.BatchNormalization())
+BarkNet1_2.add(RegularDense(14))
+BarkNet1_2.add(keras.layers.BatchNormalization())
+BarkNet1_2.add(RegularDense(7))
+BarkNet1_2.add(keras.layers.Dropout(0.3))
+BarkNet1_2.add(keras.layers.Dense(5, activation="softmax",
+                                  kernel_initializer="glorot_uniform"))
