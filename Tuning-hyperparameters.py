11611b02e49bf8a5d588cb71745e2e13f21fad98
Kuba <75746226+Whisperer8526@users.noreply.github.com>
2021-05-05 11:17:46 +0200

    Update Tuning-hyperparameters.py

--- Tuning-hyperparameters.py
+++ Tuning-hyperparameters.py
@@ -2,19 +2,21 @@ import scipy                                                            #Randomi
 from sklearn.svm import SVC  
 from sklearn.metrics import classification_report, confusion_matrix 
 from sklearn.model_selection import RandomizedSearchCV
-param_grid = {'C': scipy.stats.expon(scale=100), 
-              'gamma': scipy.stats.expon(scale=.1),
+
+param_grid = {'C': scipy.stats.expon(scale = 100), 
+              'gamma': scipy.stats.expon(scale = .1),
               'kernel': ['rbf']}
-random_search = RandomizedSearchCV(SVC(),param_grid,refit=True,verbose=2, cv=3)
+random_search = RandomizedSearchCV(SVC(), param_grid, refit=True, verbose=2, cv=3)
 random_search.fit(scaled_X_train, y_train)
 
 
 
-from sklearn.svm import SVC                                             #Randomized Search Cross-validation
+from sklearn.svm import SVC                                             #Grid Search Cross-validation
 from sklearn.metrics import classification_report, confusion_matrix 
 from sklearn.model_selection import GridSearchCV
-param_grid = {'C': [0.1,1, 10, 100], 
-              'gamma': [1,0.1,0.01,0.001],
+
+param_grid = {'C': [0.1, 1, 10, 100], 
+              'gamma': [1, 0.1, 0.01, 0.001],
               'kernel': ['rbf']}
-grid = GridSearchCV(SVC(),param_grid,refit=True,verbose=2, cv=3)
+grid = GridSearchCV(SVC(), param_grid, refit=True, verbose=2, cv=3)
 grid.fit(scaled_X_train, y_train)
